158d597ae4e33f43c7bb4313a2e4e3bfd7911cb9
Justin Braaten <jstnbraaten@gmail.com>
2017-07-26 14:52:17 -0700

    added option to clip the out raster by the feature boundary line

--- subset_raster_by_vector_extent.py
+++ subset_raster_by_vector_extent.py
@@ -9,17 +9,24 @@ from osgeo import ogr
 import subprocess
 
 
-# Get a Layer's Extent
+######################################################################################################################
 inShape = '/vol/v2/stem/multi_lt_test/spatial/conus_tiles_northeast_epsg5070.geojson'
 inRaster = '/vol/v1/general_files/datasets/spatial_data/nlcd/nlcd_2001_v2/nlcd_2001_landcover_3x3_equal.tif'
 outRaster = '/vol/v2/stem/multi_lt_test/spatial/nlcd_2001_landcover_3x3_equal_ne.tif'
+clipRaster = 'true'
+######################################################################################################################
 
-inDriver = ogr.GetDriverByName('GeoJSON')
-inDataSource = inDriver.Open(inFile, 0)
-inLayer = inDataSource.GetLayer()
-extent = inLayer.GetExtent()
 
-#projwin = '-projwin {} {} {} {} '.format(ulxy[0], ulxy[1], lrxy[0], lrxy[1])  
+
+inDriver = ogr.GetDriverByName('GeoJSON') # this needs to be smarter so that a person can provide something besides geojson
+inDataSource = inDriver.Open(inRaster, 0)
+extent = inDataSource.GetLayer().GetExtent()
+
 projwin = '-projwin {} {} {} {} '.format(extent[0], extent[3], extent[1], extent[2])  
 cmd = 'gdal_translate -of GTiff -tr 30 30 ' + projwin + inRaster +' '+ outRaster
 subprocess.call(cmd, shell=True)
+
+if clipRaster.lower() == 'true':
+  cmd = 'gdal_rasterize -i -burn 0 '+inShape+' '+outRaster
+  subprocess.call(cmd, shell=True)
+
